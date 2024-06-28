b0cc57aa287b1aa6303c6b1a341c70cfe896bcb6
Samuel Lettnin <samueltt.dev@gmail.com>
2024-06-28 20:25:19 -0300

    a

--- curso algoritmos/exercicios/condicional/operadora.py
+++
@@ -1,8 +0,0 @@
-minutos = int(input("Digite a quantidade de minutos: "))
-
-plano = (50.0)
-
-if minutos > 100:
-    plano = plano + (minutos - 100) * 2.0
-
-print(f"valor a pagar = R$ {plano:.2f}")
--- curso algoritmos/exercicios/sequencial/troco.py
+++
@@ -1,9 +0,0 @@
-preco = float(input("Preco unitario do produto: "))
-quantidade = float(input("Quantidade comprada: "))
-dinheiro = float(input("dinheiro recebido: "))
-
-troco = dinheiro - (preco * quantidade)
-
-
-print (f"TROCO = {troco:.2f}")
- 
